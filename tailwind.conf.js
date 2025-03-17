abc77bec5560d02028fce0d1e6ebcf602d73ae78
Juneyd_Ganbarov <131536769+djuneyd@users.noreply.github.com>
2025-03-17 15:32:38 +0400

    Create tailwind.conf.js

---
+++ tailwind.conf.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff5733",  // Основной цвет (можно поменять)
+        secondary: "#3498db", // Дополнительный цвет
+        background: "#1a1a1a", // Цвет фона
+        text: "#ffffff" // Цвет текста
+      },
+    },
+  },
+  plugins: [],
+};
